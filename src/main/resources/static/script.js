f85a785ea1da2aaee0d39d6816346e1d1e9b9ede
Ritesh Rao <ritesh_rao@thbs.india.com>
2024-02-01 11:20:07 +0530

    update update button

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,14 @@
+function openModal() {
+    document.getElementById('myModal').style.display = 'flex';
+}
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+// Function to submit the form (you can replace this with your logic)
+function submitForm() {
+    alert('Form submitted!');
+    closeModal();
+}
